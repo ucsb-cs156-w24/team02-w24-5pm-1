d489e786fb9f37cb30865f51a5bd175751515acb
ChrisWoolson <cw.woolson@gmail.com>
2024-02-08 15:50:16 -0800

    added entity class and repository class

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -3,9 +3,9 @@ package edu.ucsb.cs156.example.entities;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +16,15 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "ucsbdates")
-public class UCSBDate {
+@Entity(name = "RecommendationRequest")
+public class RecommendationRequest {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-
-  private String quarterYYYYQ;
-  private String name;
-  private LocalDateTime localDateTime;
+  // private Long id;
+  private String requesterEmail;
+  private String professorEmail;
+  private String explanation;
+  private LocalDateTime dateRequested;
+  private LocalDateTime dateNeeded;
+  private boolean done;
 }
