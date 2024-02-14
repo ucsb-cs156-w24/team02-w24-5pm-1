ccd94a62318bc44a0b7e71d87bd6c47711e39249
Bravo-Luis <luisbravo@ucsb.edu>
2024-02-13 17:08:04 -0800

    fixed import ucsborganizationrepository

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -1,3 +1,5 @@
+package edu.ucsb.cs156.example.repositories;
+
 import edu.ucsb.cs156.example.entities.UCSBOrganization;
 
 import org.springframework.data.repository.CrudRepository;
@@ -5,8 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String>{
-    // public UCSBOrganization findByOrgCode(String orgCode);
-    // public UCSBOrganization findByOrgTranslationShort(String orgTranslationShort);
-    // public UCSBOrganization findByOrgTranslation(String orgTranslation);
-    // public UCSBOrganization findByInactive(boolean inactive);
+    
 } 
